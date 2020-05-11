84e779491af4cf7ed402673d2daa48b4e76fb258
micahjbartron <micahjbartron@gmail.com>
2020-05-11 15:35:48 -0600

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+import aligator from "./models/alligator.js"
+
+
+
+let austin = new aligator("Austin", "green", "male", 200, "big")
+console.log(austin.teethSize);
---
+++ models/alligator.js
@@ -0,0 +1,17 @@
+
+
+export default class Alligator {
+  constructor(
+    name = '',
+    color = '',
+    gender = '',
+    weight = 0,
+    teethSize = '',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.teethSize = teethSize;
+  }
+}
